52b800f0cfe6acd485d9e32509de97b586f7bec0
20581876 <20581876@CHY0LW0PC0GB9HE.GHS.ONEACS.COM>
2020-04-09 19:32:26 -0600

    first commit uploading all files

---
+++ forgithub/src/forgithub/Calculator.java
@@ -0,0 +1,14 @@
+package forgithub;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		code code1 = new code();
+		System.out.println(code1.sum(10, 12));
+		
+
+	}
+
+}
---
+++ forgithub/src/forgithub/code.java
@@ -0,0 +1,16 @@
+package forgithub;
+
+public class code {
+	
+	private int a;
+	private int b;
+	
+	public int sum(int a,int b){
+		this.a = a;
+		this.b = b;
+		int sum = a+b;
+		return sum;
+		
+	}
+
+}
